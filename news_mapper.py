79dd98be473ba525c538eceab996233cb4f80855
Jooah Seo <seo.jooah@gmail.com>
2021-07-08 12:58:13 -0400

    cdescription on news_mapper.py and remove print

--- news_mapper.py
+++ news_mapper.py
@@ -1,23 +1,27 @@
+"""
+    Wrap the date from the api, and send the list of news obj to frontend
+"""
+
 
 class News_List:
     """news list wrapper class"""
+
     def __init__(self):
         self.list = []
-    
 
-    def news_mapper(self,res):
+    def news_mapper(self, res):
         """map the each news to news_obj and append to the list"""
         articles = res['articles']
-        print ("********** How articles look like? ***********")
-        print(articles)
         for news in articles:
-            news_obj = News_Obj(news['url'], news['title'], news['description'], news['publishedAt'], news['urlToImage'])
+            news_obj = News_Obj(
+                news['url'], news['title'], news['description'], news['publishedAt'], news['urlToImage'])
             self.list.append(news_obj)
 
 
 class News_Obj:
     """news obj wrapper class"""
-    def __init__(self,url,title,description,date,image):
+
+    def __init__(self, url, title, description, date, image):
         self.url = url
         self.title = title
         self.description = description
